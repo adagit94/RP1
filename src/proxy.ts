8ff6f78f489e8cfda2a2ba74f05f306f08683ba6
adagit94 <56156969+adagit94@users.noreply.github.com>
2024-01-01 12:42:23 +0100

    .

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,47 +1,62 @@
 import 'dotenv/config';
-import http from 'node:http';
 import https from 'node:https';
 import fs from 'node:fs';
 import { setInterval } from 'node:timers';
-import { gate } from './gate.js';
 import { DosProtection } from './protections/DosProtection.js';
 import { ServerInfo, ServerSettings } from './types.js';
-import { parseServersSettings } from './utils.js';
+import { collectServersInfos, parseServersSettings } from './utils.js';
 
-const { SERVERS, SERVERS_CHECK_INTERVAL, CONNECTIONS_LIMIT, CONNECTION_TIMEOUT, REQ_TRANSFER_TIMEOUT } = process.env;
+const { PORT, ALLOWED_ORIGINS, SERVERS, SERVERS_CHECK_INTERVAL, CONNECTIONS_LIMIT, CONNECTION_TIMEOUT, REQ_TRANSFER_TIMEOUT, MAX_REQ_BYTES } = process.env;
 
-if (Number.isNaN(SERVERS_CHECK_INTERVAL)) throw new Error('SERVERS_CHECK_INTERVAL env. variable must be provided.');
+if (SERVERS === undefined) throw new Error('SERVERS env. variable must be provided.');
+if (Number.isNaN(SERVERS_CHECK_INTERVAL)) throw new Error('SERVERS_CHECK_INTERVAL env. variable number must be provided.');
+if (Number.isNaN(CONNECTIONS_LIMIT)) throw new Error('CONNECTIONS_LIMIT env. variable number must be provided.');
+if (Number.isNaN(CONNECTION_TIMEOUT)) throw new Error('CONNECTION_TIMEOUT env. variable number must be provided.');
+if (Number.isNaN(REQ_TRANSFER_TIMEOUT)) throw new Error('REQ_TRANSFER_TIMEOUT env. variable number must be provided.');
+if (Number.isNaN(MAX_REQ_BYTES)) throw new Error('MAX_REQ_BYTES env. variable number must be provided.');
 
+const dosProtection = new DosProtection({ connectionsLimit: Number(CONNECTIONS_LIMIT) });
+const allowedOrigins = ALLOWED_ORIGINS ? ALLOWED_ORIGINS.split(',') : undefined;
 const serversSettings: ServerSettings[] = parseServersSettings(SERVERS);
-let serversInfos: ServerInfo[] = [];
-
-export const dosProtection = CONNECTIONS_LIMIT ? new DosProtection({ connectionsLimit: Number(process.env.CONNECTIONS_LIMIT) }) : undefined;
+let serversInfos: ServerInfo[] = await collectServersInfos(serversSettings);
 
 const proxy = https
   .createServer(
     {
       key: fs.readFileSync('cert/key.pem'),
       cert: fs.readFileSync('cert/cert.pem'),
-      requestTimeout: Number.isNaN(REQ_TRANSFER_TIMEOUT) ? undefined : Number(REQ_TRANSFER_TIMEOUT),
+      requestTimeout: Number(REQ_TRANSFER_TIMEOUT),
       connectionsCheckingInterval: 1000,
     },
     (req, res) => {
       try {
-        if (dosProtection) {
-          dosProtection.addConnection();
-          res.once('close', dosProtection.subtractConnection);
-        }
+        dosProtection.addConnection();
+        res.once('close', dosProtection.subtractConnection);
 
-        if (!Number.isNaN(CONNECTION_TIMEOUT)) req.setTimeout(Number(CONNECTION_TIMEOUT));
+        req.setTimeout(Number(CONNECTION_TIMEOUT));
 
-        if (req.url === undefined) {
-          res.writeHead(400, 'Url not found.');
+        // gate
+        if (allowedOrigins && (origin === undefined || !allowedOrigins.includes(origin))) {
+          res.writeHead(403, `Access from origin ${origin} denied.`);
           res.end();
-
           return;
         }
 
-        if (!gate(req, res)) return;
+        if (!dosProtection.verify()) {
+          res.writeHead(503, `Connection refused: limit overflowed.`, {
+            'access-control-allow-origin': origin,
+          });
+          res.end();
+          return;
+        }
+
+        if (req.headers['content-length'] && Number(req.headers['content-length']) > Number(MAX_REQ_BYTES)) {
+          res.writeHead(413, `Req. size limit overflowed.`, {
+            'access-control-allow-origin': origin,
+          });
+          res.end();
+          return;
+        }
       } catch (err) {
         console.error(err);
 
@@ -52,11 +67,14 @@ const proxy = https
       }
     }
   )
-  .listen(process.env.PORT);
+  .listen(PORT);
 
-setInterval(() => {
-  let infos: ServerInfo[] = [];
-  let reqs = serversSettings.map(s => http.get());
-
-  serversInfos.push();
+setInterval(async () => {
+  serversInfos = await collectServersInfos(serversSettings);
 }, Number(SERVERS_CHECK_INTERVAL));
+
+// if (req.url === undefined) {
+//   res.writeHead(400, 'Url not found.');
+//   res.end();
+//   return;
+// }
--- src/utils.ts
+++ src/utils.ts
@@ -1,19 +1,93 @@
-import { ServerSettings } from './types.js';
+import http from 'node:http';
+import { ServerInfo, ServerSettings } from './types.js';
 import * as parsers from './parsers.js';
 
-export const parseServersSettings = (x: unknown) => {
+export const parseServersSettings = (str: string) => {
   try {
-    const str = parsers.runParser(parsers.string, x)
-    
     return parsers.runParser(
       parsers.array(
         parsers.object<ServerSettings>({
           url: parsers.string,
         })
       ),
-       JSON.parse(str)
+      JSON.parse(str)
     );
   } catch (err) {
     throw new Error(`SERVERS env. variable parsing failed\n${err}`);
   }
+};
+
+export const parseServerInfo = (jsonStr: string) => {
+  return parsers.runParser(
+    parsers.object<ServerInfo>({
+      connections: parsers.number,
+    }),
+    JSON.parse(jsonStr)
+  );
+};
+
+export const receiveJson = (res: http.IncomingMessage): Promise<string> =>
+  new Promise((resolve, reject) => {
+    if (res.headers['content-type'] !== 'application/json') {
+      reject(new Error("Content-Type header isn't application/json."));
+      return;
+    }
+
+    let dataStr = '';
+
+    res.setEncoding('utf8');
+
+    res.on('data', chunk => {
+      dataStr += chunk;
+    });
+
+    res.on('end', () => {
+      resolve(dataStr);
+    });
+
+    res.on('error', reject);
+  });
+
+export const collectServersInfos = async (serversSettings: ServerSettings[]): Promise<ServerInfo[]> => {
+  const reqs: Promise<ServerInfo>[] = serversSettings.map(
+    s =>
+      new Promise((resolve, reject) => {
+        const url = `${s.url}/info`;
+        const getErrMsg = (err: unknown) => `GET ${url}: ${err}`;
+        const req = http.get(url, async res => {
+          try {
+            if (res.statusCode === 200) {
+              const jsonStr = await receiveJson(res);
+              const info = parseServerInfo(jsonStr);
+
+              resolve(info);
+            } else {
+              throw new Error(`status code: ${res.statusCode}`);
+            }
+          } catch (err) {
+            reject(getErrMsg(err));
+          }
+        });
+
+        req.on('error', err => reject(getErrMsg(err)));
+      })
+  );
+
+  const results = await Promise.allSettled(reqs);
+  let infos: ServerInfo[] = [];
+
+  for (const result of results) {
+    switch (result.status) {
+      case 'fulfilled':
+        infos.push(result.value);
+        break;
+
+      case 'rejected':
+        console.error(result.reason);
+        // trigger some notification mechanism
+        break;
+    }
+  }
+
+  return infos;
 };
